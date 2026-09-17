bb92fdbaf1b1b4b42c926765e6bea8879825a400
AldAndrew0 <fdesantis@studenti.apuliadigital.it>
2026-09-17 15:24:47 +0200

    feat: add 'Place' entity and repository

---
+++ PhotoTrip.DAL/Entities/Place.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoTrip.DAL.Entities
+{
+    public class Place
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int RegionId { get; set; }
+        public Region Region { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string ThumbnailUrl { get; set; } = string.Empty;
+    }
+}
