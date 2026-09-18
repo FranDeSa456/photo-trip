f6dca443b49e1fb5d5179976537dceffef545116
Shadow32522 <fabio.scarpa03@gmail.com>
2026-09-18 10:35:15 +0200

    Feat: Add IReviewRepository Interface

---
+++ PhotoTrip.DAL/Repositories/Interfaces/IReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using PhotoTrip.DAL.Entities;
+
+namespace PhotoTrip.DAL.Repositories.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<IReadOnlyList<Review>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<Review?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        Task AddAsync(Review entity, CancellationToken cancellationToken = default);
+
+        void Update(Review entity);
+
+        void Remove(Review entity);
+    }
+}
