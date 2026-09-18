8f41c41ddf6fb3e96378a0f3c4421c34cce5f700
Shadow32522 <fabio.scarpa03@gmail.com>
2026-09-18 10:34:06 +0200

    Feat: Add Entity Review

---
+++ PhotoTrip.DAL/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoTrip.DAL.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public Place Place { get; set; } = null!;
+        public int PlaceId { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public int Score { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
