cc3975675216338206c61ded0962b7fd3625200d
FranDeSa456 <fdesantis@studenti.apuliadigital.it>
2026-09-18 10:09:46 +0200

    feat: seed italian regions

--- PhotoTrip.DAL/Data/PhotoDbContext.cs
+++ PhotoTrip.DAL/Data/PhotoDbContext.cs
@@ -15,6 +15,28 @@ namespace PhotoTrip.DAL.Data
 
             modelBuilder.Entity<Review>()
                 .ToTable(t => t.HasCheckConstraint("CK_Review_Rating_Range", "[Rating] >= 1 AND [Rating] <= 5"));
+
+            modelBuilder.Entity<Region>().HasData(
+                new Region { Id = 1, Name = "Abruzzo" },
+                new Region { Id = 2, Name = "Basilicata" },
+                new Region { Id = 3, Name = "Calabria" },
+                new Region { Id = 4, Name = "Campania" },
+                new Region { Id = 5, Name = "Emilia-Romagna" },
+                new Region { Id = 6, Name = "Friuli-Venezia Giulia" },
+                new Region { Id = 7, Name = "Lazio" },
+                new Region { Id = 8, Name = "Liguria" },
+                new Region { Id = 9, Name = "Lombardia" },
+                new Region { Id = 10, Name = "Marche" },
+                new Region { Id = 11, Name = "Molise" },
+                new Region { Id = 12, Name = "Piemonte" },
+                new Region { Id = 13, Name = "Puglia" },
+                new Region { Id = 14, Name = "Sardegna" },
+                new Region { Id = 15, Name = "Sicilia" },
+                new Region { Id = 16, Name = "Toscana" },
+                new Region { Id = 17, Name = "Trentino-Alto Adige" },
+                new Region { Id = 18, Name = "Umbria" },
+                new Region { Id = 19, Name = "Valle d'Aosta" },
+                new Region { Id = 20, Name = "Veneto" });
         }
     }
 }
